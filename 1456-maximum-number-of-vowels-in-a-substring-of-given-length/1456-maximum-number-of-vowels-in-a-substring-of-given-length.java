7e4e73b03c91bbd722635c4cf0d10280907906ac
manish-upadhyay12 <upadhyaymanish0654@gmail.com>
2026-09-11 07:12:37 +0530

    Time: 13 ms (72.4%), Space: 46.2 MB (91.28%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -5,24 +5,31 @@ class Solution {
        int vowel_count=0;
        for(int  i = 0;i<k;i++){
         char ch = s.charAt(i);
-        if(ch =='a' || ch== 'e' || ch == 'i' || ch =='o' || ch =='u'){
+        if(isvowel(ch)){
             vowel_count++;
         }
        }
        int max_vowel = vowel_count;
        for(int i = k;i<s.length();i++){
         char ch1 = s.charAt(left);
-        if(ch1 =='a' || ch1 == 'e' || ch1 == 'i' || ch1 =='o' || ch1 =='u'){
+        if(isvowel(ch1)){
             vowel_count--;
         }
         left++;
         char ch2 = s.charAt(i);
-            if(ch2 =='a' || ch2 == 'e' || ch2  == 'i' || ch2  =='o' || ch2 =='u'){
+            if(isvowel(ch2)){
             vowel_count++;
         }
         max_vowel = Math.max(max_vowel,vowel_count);
        
+
        }
+
+      
        return max_vowel;
+
     }
+      public static boolean isvowel( char ch){
+             return ch =='a' || ch == 'e' || ch == 'i' || ch =='o' || ch =='u';
+        }
 }
